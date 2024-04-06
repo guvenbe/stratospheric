f874f175e7a6ceaa99f8ab13d758874f159f2204
Bora Guven <guvenbe@gmail.com>
2024-04-05 23:51:33 -0400

    me

--- chapters/chapter-1/appcdk/src/main/java/com/myorg/AppcdkApp.java
+++ chapters/chapter-1/appcdk/src/main/java/com/myorg/AppcdkApp.java
@@ -4,39 +4,35 @@ import software.amazon.awscdk.App;
 import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
 
-import java.util.Arrays;
-
 public class AppcdkApp {
     public static void main(final String[] args) {
         App app = new App();
 
+      String accoountId = (String) app.getNode().tryGetContext("accountId");
+      requireNonEmpty(accoountId, "context variable 'accountId' must not be null");
+      String region = (String) app.getNode().tryGetContext("region");
+      requireNonEmpty(region, "context variable 'region' must not be null");
+
+      Environment environment = makeEnv(accoountId, region);
+
         new AppcdkStack(app, "AppcdkStack", StackProps.builder()
-                // If you don't specify 'env', this stack will be environment-agnostic.
-                // Account/Region-dependent features and context lookups will not work,
-                // but a single synthesized template can be deployed anywhere.
-
-                // Uncomment the next block to specialize this stack for the AWS Account
-                // and Region that are implied by the current CLI configuration.
-                /*
-                .env(Environment.builder()
-                        .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
-                        .region(System.getenv("CDK_DEFAULT_REGION"))
-                        .build())
-                */
-
-                // Uncomment the next block if you know exactly what Account and Region you
-                // want to deploy the stack to.
-                /*
-                .env(Environment.builder()
-                        .account("123456789012")
-                        .region("us-east-1")
-                        .build())
-                */
-
-                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+          .env(environment)
                 .build());
 
         app.synth();
     }
+
+  static Environment makeEnv(String account, String region) {
+    return Environment.builder()
+      .account(account)
+      .region(region)
+      .build();
+  }
+
+  public static void requireNonEmpty(String value, String message) {
+    if (value == null || value.isEmpty()) {
+      throw new IllegalArgumentException(message);
+    }
+  }
 }
 
--- chapters/chapter-1/appcdk/src/main/java/com/myorg/AppcdkStack2.java
+++ chapters/chapter-1/appcdk/src/main/java/com/myorg/AppcdkStack2.java
@@ -1,24 +1,19 @@
 package com.myorg;
 
+import dev.stratospheric.cdk.DockerRepository;
 import software.amazon.awscdk.Environment;
-import software.amazon.awscdk.services.ecr.LifecycleRule;
-import software.amazon.awscdk.services.ecr.Repository;
-import software.amazon.awscdk.services.ecr.TagMutability;
-import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-import dev.stratospheric.cdk.DockerRepository;
-
-import java.util.Arrays;
+import software.constructs.Construct;
 // import software.amazon.awscdk.Duration;
 // import software.amazon.awscdk.services.sqs.Queue;
 
-public class AppcdkStack extends Stack {
-    public AppcdkStack(final Construct scope, final String id) {
+public class AppcdkStack2 extends Stack {
+    public AppcdkStack2(final Construct scope, final String id) {
         this(scope, id, null);
     }
 
-    public AppcdkStack(final Construct scope, final String id, final StackProps props) {
+    public AppcdkStack2(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
 //        Repository repository = Repository.Builder.create(this, "repository")
